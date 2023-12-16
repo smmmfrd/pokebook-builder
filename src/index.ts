a0e719f2e259f9f551027eae2ca6d8060cb4044a
smmmfrd <smmmfrd@gmail.com>
2023-12-15 17:56:26 -0800

    Prisma Connection

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,17 @@
 // const pokemonData = await JSON.parse(pokemonText);
 // console.log(pokemonData[0]);
 
+import { PrismaClient } from "@prisma/client";
 import { BotData } from "./bots";
 
 const date = new Date().getDate();
 console.log(date);
 
 BotData();
+
+// Test prisma connection...
+const prisma = new PrismaClient();
+const lastPost = await prisma.post.findFirst({
+  where: { poster: { bot: true } },
+});
+console.log(lastPost);
