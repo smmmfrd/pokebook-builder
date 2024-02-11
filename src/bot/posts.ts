58efb0c1720713f6ff043ebf64723e7ecb2f1201
smmmfrd <smmmfrd@gmail.com>
2024-02-11 11:16:55 -0800

    Remove Redudant Moment Creations

--- src/bot/posts.ts
+++ src/bot/posts.ts
@@ -1,11 +1,12 @@
 import { Pokemon } from "@prisma/client";
 import { PostData } from "../types";
-import moment from "moment";
+import { Moment } from "moment";
 import { randomPostContent } from "./utils";
 
 export function randomPosts(
   randBot: () => Pokemon,
-  postNumber: number
+  postNumber: number,
+  firstPostTime: Moment
 ): PostData[] {
   return Array.from({ length: postNumber }, (_, i) => {
     const poke = randBot();
@@ -14,9 +15,7 @@ export function randomPosts(
     return {
       posterId: poke.id,
       content,
-      createdAt: moment()
-        .subtract(7 - i, "hours")
-        .toDate(),
+      createdAt: firstPostTime.subtract(7 - i, "hours").toDate(),
     };
   });
 }
--- src/bots.ts
+++ src/bots.ts
@@ -1,6 +1,7 @@
 import { Pokemon, PrismaClient } from "@prisma/client";
-import { randomPosts } from "./bot/posts";
 import moment from "moment";
+
+import { randomPosts } from "./bot/posts";
 import { botLikes } from "./bot/likes";
 
 export async function BotData(prisma: PrismaClient) {
@@ -9,21 +10,21 @@ export async function BotData(prisma: PrismaClient) {
   const botData = (await JSON.parse(botText)) as Pokemon[];
   console.log(`${botData.length} Bots`);
 
-  const timeSinceLast = moment().subtract(8, "hours").toDate();
+  const timeSinceLast = moment().subtract(8, "hours");
 
   // Get a random bot pokemon and remove it from the array
   const randBot = (): Pokemon => {
     return botData.splice(Math.floor(Math.random() * botData.length), 1)[0];
   };
 
-  const botPosts = randomPosts(randBot, 8);
+  const botPosts = randomPosts(randBot, 8, timeSinceLast);
 
   await prisma.post.createMany({ data: botPosts });
   // console.log(botPosts);
 
   // Need to query the database with the new bot posts included as to get their id's
   const newPosts = await prisma.post.findMany({
-    where: { createdAt: { gt: timeSinceLast } },
+    where: { createdAt: { gt: timeSinceLast.toDate() } },
     orderBy: { createdAt: "desc" },
     select: {
       id: true,
