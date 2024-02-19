7b5b364cde90e435eaf4560e235d164bc5877134
smmmfrd <smmmfrd@gmail.com>
2024-02-19 09:18:25 -0800

    Add Timer

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 import { BotData } from "./bots";
+const startTime = performance.now();
 
 const prisma = new PrismaClient();
 
 await BotData(prisma);
+
+const endTime = performance.now();
+
+console.log(`Took ${endTime - startTime}ms`);
